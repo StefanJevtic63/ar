ab5dd0e1abc879cccb65be1d326e7fdcb2c5dbdf
Stefan Jevtic <stefanjevtic10@yahoo.com>
2024-11-13 02:36:40 +0100

    chore: added test script
    10 input tests that are being run with shell script

---
+++ perform-tests.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Zaustavljanje izvršavanja skripte u slučaju greške
+set -e
+
+# Prevođenje programa
+g++ -o dp_algorithm main.cpp
+
+# Provera rezultata prevođenja
+if [ $? -ne 0 ]; then
+  echo "Kompajliranje nije uspelo!"
+  exit 1
+fi
+
+# Folder sa test primerima
+input_dir="test-cases-in"
+output_dir="test-cases-out"
+
+# Kreiraj direktorijum ukoliko vec ne postoji
+if [ ! -d "$output_dir" ]; then
+  mkdir -p "$output_dir"
+fi
+
+# Pokretanje DP algoritma za svaki test primer
+for i in {1..10}; do
+  input_file="${input_dir}/test${i}-in.txt"
+  output_file="${output_dir}/test${i}-out.txt"
+
+  # Provera postojanja ulazne datoteke
+  if [ -f "$input_file" ]; then
+    # Prosleđivanje sadržaja ulazne datoteke na ulaz programu
+    # i upisivanje izlaza u izlaznu datoteku
+    ./dp_algorithm < "$input_file" > "$output_file"
+  fi
+done
+
+echo "Testiranje je završeno. Rezultati su smešteni u folderu 'test-cases'."
